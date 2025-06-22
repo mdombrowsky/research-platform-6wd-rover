1fb18502bc1e4cb745816fa06aeb6d1d8bd29485
mdombrowsky <mdombrowsky@unm.edu>
2025-06-21 21:28:11 -0700

    Update due.ino

--- Sensor Interface Module/firmware/Arduino DUE/due/due.ino
+++ Sensor Interface Module/firmware/Arduino DUE/due/due.ino
@@ -148,9 +148,39 @@
 
 // move the Int32Publisher here and rename previous class to agent_handler
 
+class Int32Publisher
+{
+public:
+    Int32Publisher() : count_(0), state_(UNINITIALIZED) {}
+
+    bool setup(rclc_support_t* support, rcl_node_t* node, rcl_allocator_t* allocator)
+    {
+        allocator_ = allocator;
+    RCCHECK(rclc_publisher_init_best_effort(
+    &publisher_, node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+    "std_msgs_msg_Int32"));
+    return (state_ = INITIALIZED), true;
+  }
+
+  void publish()
+    {
+        if (state_ != INITIALIZED) return;
+    std_msgs__msg__Int32 msg;
+    msg.data = count_++;
+    rcl_publish(&publisher_, &msg, nullptr);
+  }
+
+private:
+  enum State { UNINITIALIZED, INITIALIZED };
+  rcl_publisher_t publisher_;
+  rcl_allocator_t* allocator_;
+  State state_;
+  int count_;
+};
 
 //////////////////////////////////////////////////////////////////////////
-// Test Publisher (MinimalPublisher )
+// Test Publisher (MinimalPublisher)
 //////////////////////////////////////////////////////////////////////////
 
 class MinimalPublisher
@@ -230,6 +260,53 @@ private:
     }
 };
 
+
+//////////////////////////////////////////////////////////////////////////
+// Log Publisher
+//////////////////////////////////////////////////////////////////////////
+
+class LogPublisher
+{
+  public:
+  LogPublisher() {
+    memset(&msg_, 0, sizeof(msg_));
+  }
+
+  bool setup(rcl_node_t* node) {
+    rcl_publisher_options_t pub_ops = rcl_publisher_get_default_options();
+    pub_ops.qos.reliability = RMW_QOS_POLICY_RELIABILITY_RELIABLE;
+    pub_ops.qos.durability = RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL;
+
+    return rcl_publisher_init(
+    &publisher_,
+    node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(rcl_interfaces, msg, Log),
+    "/rosout",
+    &pub_ops) == RCL_RET_OK;
+  }
+
+  void log(const char* file, const char* func, int line, int level, const char* fmt, ...) {
+    char buffer[128];
+    va_list args;
+    va_start(args, fmt);
+    vsnprintf(buffer, sizeof(buffer), fmt, args);
+    va_end(args);
+
+    rosidl_runtime_c__String__assign(&msg_.name, "AgentHandler");
+    rosidl_runtime_c__String__assign(&msg_.msg, buffer);
+    rosidl_runtime_c__String__assign(&msg_.file, file);
+    rosidl_runtime_c__String__assign(&msg_.function, func);
+    msg_.line = line;
+    msg_.level = level;
+
+    rcl_publish(&publisher_, &msg_, nullptr);
+  }
+
+  private:
+  rcl_publisher_t publisher_;
+  rcl_interfaces__msg__Log msg_;
+};
+
 //////////////////////////////////////////////////////////////////////////
 // Custom Memory Allocator
 //////////////////////////////////////////////////////////////////////////
@@ -271,24 +348,16 @@ private:
 class AgentHandler
 {
 public:
-
-    // Constructor
-    AgentHandler() : msg_{}, state_(WAITING_AGENT)
+    AgentHandler(MinimalPublisher* publisher, LogPublisher* logger)
+        : state_(WAITING_AGENT), publisher_(publisher), logger_(logger)
     {
-        instance_ = this;  // Set static instance pointer
-    }
-
-    // Destructor
-    ~AgentHandler()
-    {
-        destroy_entities();
+        instance_ = this;
     }
 
     void setup()
     {
         set_microros_transports();
         pinMode(LED_PIN, OUTPUT);
-        msg_.data = 0;
     }
 
     void loop()
@@ -296,29 +365,26 @@ public:
         switch (state_)
         {
         case WAITING_AGENT:
-            EXECUTE_EVERY_N_MS(500,
-                {
-                    state_ = (RMW_RET_OK == rmw_uros_ping_agent(100, 1)) ? AGENT_AVAILABLE : WAITING_AGENT;
+            EXECUTE_EVERY_N_MS(500, {
+                state_ = (RMW_RET_OK == rmw_uros_ping_agent(100, 1)) ? AGENT_AVAILABLE : WAITING_AGENT;
                 });
             break;
+
         case AGENT_AVAILABLE:
             state_ = create_entities() ? AGENT_CONNECTED : WAITING_AGENT;
-            if (state_ == WAITING_AGENT)
-            {
-                destroy_entities();
-            }
+            if (state_ == WAITING_AGENT) destroy_entities();
             break;
+
         case AGENT_CONNECTED:
-            EXECUTE_EVERY_N_MS(200,
-                {
-                    state_ = (RMW_RET_OK == rmw_uros_ping_agent(100, 1)) ? AGENT_CONNECTED : AGENT_DISCONNECTED;
+            EXECUTE_EVERY_N_MS(200, {
+                state_ = (RMW_RET_OK == rmw_uros_ping_agent(100, 1)) ? AGENT_CONNECTED : AGENT_DISCONNECTED;
                 });
-            if (state_ == AGENT_CONNECTED)
-            {
-                // spin nodes here
+
+            if (state_ == AGENT_CONNECTED) {
                 rclc_executor_spin_some(&executor_, RCL_MS_TO_NS(100));
             }
             break;
+
         case AGENT_DISCONNECTED:
             destroy_entities();
             state_ = WAITING_AGENT;
@@ -329,65 +395,28 @@ public:
     }
 
 private:
-    enum State
-    {
-        WAITING_AGENT,
-        AGENT_AVAILABLE,
-        AGENT_CONNECTED,
-        AGENT_DISCONNECTED
-    };
-
+    enum State { WAITING_AGENT, AGENT_AVAILABLE, AGENT_CONNECTED, AGENT_DISCONNECTED };
+    State state_;
     rclc_support_t support_;
     rcl_node_t node_;
-    rcl_timer_t timer_;
     rclc_executor_t executor_;
     rcl_allocator_t allocator_;
-    rcl_publisher_t publisher_;
-    std_msgs__msg__Int32 msg_;
-    State state_;
+    rcl_timer_t timer_;
 
-    rcl_publisher_t log_publisher_;
-    std_msgs__msg__String log_msg_;
+    MinimalPublisher* publisher_;
+    LogPublisher* logger_;
 
-    rcl_interfaces__msg__Log rosout_msg_;
-
-    static AgentHandler* instance_;  // Static instance pointer
+    static AgentHandler* instance_;
 
     static void timer_callback(rcl_timer_t* timer, int64_t last_call_time)
     {
-        (void)timer;
-        (void)last_call_time;
-        if (instance_)
-        {
-            instance_->publish_message();
-        }
+        if (instance_) instance_->on_timer();
     }
 
-    void publish_message()
+    void on_timer()
     {
-        rcl_publish(&publisher_, &msg_, nullptr);
-        msg_.data++;
-
-        publish_log("Test log message number %d", msg_.data);
-    }
-
-    void publish_log(const char* format, ...)
-    {
-        char buffer[128];
-        va_list args;
-        va_start(args, format);
-        vsnprintf(buffer, sizeof(buffer), format, args);
-        va_end(args);
-
-        // Assign strings - use rosidl_runtime_c__String__assign to manage memory properly
-        rosidl_runtime_c__String__assign(&rosout_msg_.name, "AgentHandler");
-        rosidl_runtime_c__String__assign(&rosout_msg_.msg, buffer);
-        rosidl_runtime_c__String__assign(&rosout_msg_.file, __FILE__);
-        rosidl_runtime_c__String__assign(&rosout_msg_.function, __func__);
-        rosout_msg_.line = __LINE__;
-        rosout_msg_.level = RCL_LOG_SEVERITY_INFO; // or other severity
-
-        rcl_publish(&log_publisher_, &rosout_msg_, nullptr);
+        // This would now delegate to external publisher logic
+        logger_->log(__FILE__, __func__, __LINE__, RCL_LOG_SEVERITY_INFO, "Agent is alive");
     }
 
     bool create_entities()
@@ -395,64 +424,43 @@ private:
         allocator_ = rcl_get_default_allocator();
 
         RCCHECK(rclc_support_init(&support_, 0, nullptr, &allocator_));
-        RCCHECK(rclc_node_init_default(&node_, "uROS_Agent", "", &support_));       //node name
-        RCCHECK(rclc_publisher_init_best_effort(
-            &publisher_,
-            &node_,
-            ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-            "std_msgs_msg_Int32"));
+        RCCHECK(rclc_node_init_default(&node_, "uros_node", "", &support_));
 
-        const unsigned int timer_timeout = 1000;
-        RCCHECK(rclc_timer_init_default(
-            &timer_,
-            &support_,
-            RCL_MS_TO_NS(timer_timeout),
-            timer_callback));
+        if (!publisher_->setup(&support_, &node_, &allocator_)) return false;
+        if (!logger_->setup(&node_)) return false;
 
         executor_ = rclc_executor_get_zero_initialized_executor();
         RCCHECK(rclc_executor_init(&executor_, &support_.context, 1, &allocator_));
+        RCCHECK(rclc_timer_init_default(&timer_, &support_, RCL_MS_TO_NS(1000), timer_callback));
         RCCHECK(rclc_executor_add_timer(&executor_, &timer_));
-
-        //for log-publisher
-        rcl_publisher_options_t pub_ops = rcl_publisher_get_default_options();
-        pub_ops.qos.reliability = RMW_QOS_POLICY_RELIABILITY_RELIABLE;
-        pub_ops.qos.durability = RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL;
-        rcl_ret_t ret = rcl_publisher_init(
-            &log_publisher_,
-            &node_,
-            ROSIDL_GET_MSG_TYPE_SUPPORT(rcl_interfaces, msg, Log),
-            "/rosout",
-            &pub_ops);
-        if (ret != RCL_RET_OK)
-        {
-            // handle error
-        }
-        rosout_msg_.name.data = nullptr;
-        rosout_msg_.msg.data = nullptr;
-        rosout_msg_.file.data = nullptr;
-        rosout_msg_.function.data = nullptr;
-        rosout_msg_.line = 0;
-        rosout_msg_.level = RCL_LOG_SEVERITY_INFO; // example
 
         return true;
     }
 
     void destroy_entities()
     {
-        rmw_context_t* rmw_context = rcl_context_get_rmw_context(&support_.context);
-        (void)rmw_uros_set_context_entity_destroy_session_timeout(rmw_context, 0);
-
-        (void)rcl_publisher_fini(&publisher_, &node_);
-        (void)rcl_timer_fini(&timer_);
         rclc_executor_fini(&executor_);
-        (void)rcl_node_fini(&node_);
+        rcl_timer_fini(&timer_);
+        rcl_node_fini(&node_);
         rclc_support_fini(&support_);
-
-        (void)rcl_publisher_fini(&log_publisher_, &node_);
     }
-
-
 };
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////
+// MAIN Hardware Manager
+//////////////////////////////////////////////////////////////////////////
+
+/*
+void xxx()
+{
+  // check winbond
+  // listen to lora5 for shutdown
+}
+*/
 
 
 //////////////////////////////////////////////////////////////////////////
@@ -460,11 +468,23 @@ private:
 //////////////////////////////////////////////////////////////////////////
 
 // Initialize static member
-AgentHandler* AgentHandler::instance_ = nullptr;
-AgentHandler publisher_node;
+//AgentHandler* AgentHandler::instance_ = nullptr;
+//AgentHandler publisher_node;
 
+// Static instance definition
+//AgentHandler* AgentHandler::instance_ = nullptr;
+
+//MinimalPublisher* MinimalPublisher::instance_ = nullptr;
+//MinimalPublisher minimal_pub;
+
+// Static member initializations
 MinimalPublisher* MinimalPublisher::instance_ = nullptr;
+AgentHandler* AgentHandler::instance_ = nullptr;
+
+// Instances
 MinimalPublisher minimal_pub;
+LogPublisher log_pub;
+AgentHandler publisher_node(&minimal_pub, &log_pub);
 
 
 //////////////////////////////////////////////////////////////////////////
